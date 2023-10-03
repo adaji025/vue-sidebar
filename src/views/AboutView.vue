ffc42c21f2cba6157a0b0e5bda27aca7e4d51301
Adaji Mukhtar <53816067+adaji025@users.noreply.github.com>
2023-10-03 22:03:50 +0100

    added vue router

---
+++ src/views/AboutView.vue
@@ -0,0 +1,14 @@
+<script>
+export default {};
+</script>
+
+<template lang="">
+  <div class="container">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi
+    perspiciatis architecto ut libero optio minima similique hic maxime quas
+    earum. Illo ad exercitationem omnis laboriosam vero nulla enim aliquid
+    recusandae!
+  </div>
+</template>
+
+<style lang=""></style>
