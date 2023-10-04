2843cd482a430edec66c200575c38e3a84d6517f
Adaji Mukhtar <53816067+adaji025@users.noreply.github.com>
2023-10-04 19:06:40 +0100

    set up a master layout for dashboard

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,12 @@
 <script setup>
 import { RouterView, RouterLink } from "vue-router";
+import Header from "./components/Header.vue";
 </script>
 
 <template>
-  <main>
-    <div class="links">
-      <RouterLink active-class="active" to="/">Home</RouterLink>
-      <RouterLink active-class="active" to="/about">About</RouterLink>
-    </div>
-    <RouterView />
-  </main>
+  <!-- <Header /> -->
+
+  <RouterView />
 </template>
 
-<style scoped>
-.links {
-  padding: 20px;
-  font-size: 20px;
-  font-weight: 500;
-}
-
-.links a {
-  margin: 0 5px;
-}
-.active {
-  font-weight: bold;
-  color: green;
-  border-bottom: 2px solid green;
-}
-</style>
+<style scoped></style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,28 @@
-<template>
-  <div class="container">
-    <h1>The Car</h1>
-  </div>
-</template>
 <script>
-export default {};
+import { RouterLink } from "vue-router";
 </script>
-<style></style>
+
+<template>
+ <div class="w-full bg-gray-100">
+  <div class="flex justify-between items-center h-[50px]">
+    header
+  </div>
+ </div>
+</template>
+
+<style>
+.links {
+  padding: 20px;
+  font-size: 20px;
+  font-weight: 500;
+}
+
+.links a {
+  margin: 0 5px;
+}
+.active {
+  font-weight: bold;
+  color: green;
+  border-bottom: 2px solid green;
+}
+</style>
